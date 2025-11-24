a3ab4028d2ea250a7f565d0981c8bda1f4a2f494
Wesleyhrl <wesley_henrique15@hotmail.com>
2025-11-24 06:33:31 -0300

    UPDATE 4_frontend: ajustar a URL da API

--- 4_frontend/sentiment-analysis-frontend/src/app/actions/navigation.ts
+++ 4_frontend/sentiment-analysis-frontend/src/app/actions/navigation.ts
@@ -7,15 +7,10 @@ interface CategoryItem {
 
 export async function fetchNavigation(filtro: string = ''): Promise<CategoryItem[]> {
   try {
-    // Substitua pela URL correta da sua API Python se estiver em Docker ou outra porta
-    const apiUrl = `http://127.0.0.1:8000/produtos/navegacao/?filtro=${filtro}`;
+    const API_BASE_URL = process.env.API_BASE_URL
+    const apiUrl = `${API_BASE_URL}/produtos/navegacao/?filtro=${filtro}`;
     
-    const response = await fetch(apiUrl, {
-      cache: 'no-store', // Garante dados sempre frescos
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    const response = await fetch(apiUrl);
 
     if (!response.ok) {
       throw new Error('Falha ao buscar categorias');
