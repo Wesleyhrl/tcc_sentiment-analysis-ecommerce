8627fcd024f9b23a8f15a7f2a7e42808e80b8c87
Wesleyhrl <wesley_henrique15@hotmail.com>
2025-12-01 03:41:29 -0300

    ADD 4_frontend: adição de metadados de informação do site

--- 4_frontend/sentiment-analysis-frontend/src/app/layout.tsx
+++ 4_frontend/sentiment-analysis-frontend/src/app/layout.tsx
@@ -1,5 +1,37 @@
 import "./globals.css";
 import Header from './../components/header/index';
+import type { Metadata } from 'next';
+
+const baseUrl = process.env.NEXT_PUBLIC_SITE_URL 
+  ? `https://${process.env.NEXT_PUBLIC_SITE_URL}` 
+  : 'http://localhost:3000/';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
+  title: {
+    default: "Review Sentimentum | Análise de Sentimentos com IA",
+    template: "%s | Review Sentimentum",
+  },
+  description: "Plataforma para análise de sentimentos em avaliações de e-commerce utilizando Processamento de Linguagem Natural (NLP) e Inteligência Artificial.",
+  keywords: ["Análise de Sentimentos", "NLP", "IA", "TCC", "E-commerce", "Reviews", "React","Next.js","TailwindCSS"],
+  authors: [{ name: "Wesley Henrique", url: "https://github.com/Wesleyhrl" }],
+  creator: "Wesley Henrique",
+  icons: {
+    icon: "/icon.svg",
+  },
+  openGraph: {
+    title: "Review Sentimentum - Análise de Sentimentos",
+    description: "Descubra a análise de sentimentos em avaliações de e-commerce usando Inteligência Artificial.",
+    siteName: "Review Sentimentum",
+    locale: "pt_BR",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+};
+
 
 
 
