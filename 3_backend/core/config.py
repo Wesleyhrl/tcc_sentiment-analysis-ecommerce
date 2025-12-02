42f8f542b719b384ac17eee989937b3743e02bc9
Wesleyhrl <wesley_henrique15@hotmail.com>
2025-12-02 14:15:26 -0300

    UPDATE 3_backend: configurações de variáveis e conexões

--- 3_backend/core/config.py
+++ 3_backend/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    MONGODB_URL = os.getenv("MONGODB_URL")
-    MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
+    MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017/")
+    MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME", "produtos_analises")
 
 settings = Settings()
